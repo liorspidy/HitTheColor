8c598167135ded6c35a573b0ccb25ce81d6ea0ff
Lior Fridman <liorspidy@gmail.com>
2022-09-03 15:17:13 +0300

    test

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const timeSet = document.querySelector('#setTime');
 const blocksColor = '#FAD897';
 let dotColors = null;
 let TIME_LIMIT = 60;
+let x=0;
 
 function changeDotColors() {
   dotColors = setInterval(() => {
